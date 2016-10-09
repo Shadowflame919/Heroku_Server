607c8021bca8419c0a555a2da52b31fced062b0d
Matty <shadowflame919@gmail.com>
2016-10-09 21:15:38 +1100

    Add files via upload

--- client.js
+++ client.js
@@ -1,11 +1,42 @@
 var socket = io();
+var username = undefined;
+var user_scores = undefined;
 
-$('form').submit(function(){
-	socket.emit('send message', $('#m').val());
-	$('#m').val('');
+$('form').submit(function(){	// When user sumbits name
+	socket.emit('submit username', $('#m').val());
+	username = $('#m').val();
+	$('#form-div').hide();
+	$('#user-text').show();
 	return false;
 });
 
-socket.on('distribute message', function(msg){
-	$('#messages').append($('<li>').text(msg));
+socket.on('user scores', function(scoreList){
+	user_scores = scoreList;
+	renderLeaderboard();
+	if (username != undefined) renderText();
 });
+
+function renderText() {
+	textString = "";
+	textString += "Playing as " + username + "<br>";
+	textString += "Points: " + user_scores[username];
+	document.getElementById("text").innerHTML = textString;
+}
+
+function renderLeaderboard() {
+	textString = "";
+	keyList = Object.keys(user_scores);
+	for (var i=0; i<keyList.length && i<10; i++) {
+		textString += (i+1) + ". " + keyList[i] + ": " + user_scores[keyList[i]] + "<br>";
+	}
+	document.getElementById("leaderboard").innerHTML = textString;
+}
+
+function pressButton() {
+	user_scores[username] += 1;
+	renderText();
+}
+
+function saveScore() {
+	socket.emit('save score', {name:username, score:user_scores[username]});
+}
--- server.js
+++ server.js
@@ -2,25 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res){	// If client asks for "/" then they want the html file
 	res.sendFile(__dirname + '/client.html');
 });
-app.get('/client.js', function(req, res){
+app.get('/client.js', function(req, res){	// If client asks for "/client.js" then they want the js file
 	res.sendFile(__dirname + '/client.js');
 });
 
-io.on('connection', function(socket){
-	console.log("user connected...")
+io.on('connection', function(socket){	// When client connects to server
+	console.log("user connected...");
+	socket.emit('user scores', user_list);	// Give user score list after connecting
+	
+	socket.on('submit username', function(name){
+		console.log(name + ' is now playing');
+		if (user_list[name] == undefined) user_list[name] = 0;
+		socket.emit('user scores', user_list);	// After logging in client should be given updated leaderboard with their own username inside
+	});
+	
 	socket.on('disconnect', function(){
 		console.log('user disconnected...');
 	});
 	
-	socket.on('send message', function(msg){
-		io.emit('distribute message', msg);
+	socket.on('button pressed', function(){
+		console.log("Button Pressed");
+		number ++;
+		io.emit('new number', number);
 	});
 	
-	socket.on('move', function(dir){
-		console.log("Moved in dir " + dir)
+	socket.on('save score', function(userStats){
+		user_list[userStats.name] = userStats.score;
+		socket.emit('user scores', user_list);
 	});
 });
 
@@ -29,3 +40,5 @@ var port = Number(process.env.PORT || 8080);
 http.listen(port, '0.0.0.0', function(){
 	console.log('listening on *:8080');
 });
+
+user_list = {};
